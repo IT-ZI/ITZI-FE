a38b4f76fd1355f9354240edc20ace7279b604a0
Yunsejin <yellowsejin@naver.com>
2025-06-02 14:54:02 +0900

    commit

---
+++ Desktop/2025/wagi/week08/task/week08_task/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int bool isPrime(int number) {
+    if (number <= 1) return false;
+    if (number == 2) return true;
+    if (number % 2 == 0) return false; 
+
+    for (int i = 3; i * i <= number; i += 2) {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cout << "정수를 입력하세요: ";
+    cin >> n;
+
+    if (isPrime(n))
+        cout << n << "은(는) 소수입니다." << endl;
+    else
+        cout << n << "은(는) 소수가 아닙니다." << endl;
+
+    return 0;
+}
