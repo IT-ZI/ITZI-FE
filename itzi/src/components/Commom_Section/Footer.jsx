207a683682fd9c9c1779a35a9b925c45088cf800
taeyoung0524 <miseoseo1@naver.com>
2025-08-16 00:45:30 +0900

    FEAT : footer 기초 세팅

--- itzi/src/components/Commom_Section/Footer.jsx
+++ itzi/src/components/Commom_Section/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import styles from "../../assets/scss/section/footer.module.scss"
+import instargram from "../../assets/img/instargram.png"
+import blog from "../../assets/img/blog.png"
 
 const Footer = () => {
   return (
     <div className='body'>
       <div className={styles.container}>
-        안녕
+        <div className={styles.top}>
+            <p>이용가이드</p>
+            <p>고객문의</p>
+        </div>
+        <div className={styles.bottom}>
+            <img src={instargram} alt="" />
+            <img src={blog} alt="" />
+        </div>
       </div>
     </div>
   )
