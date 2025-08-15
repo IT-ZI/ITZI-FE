af8e9841b2ffdad4d61d3b1f1d4b6c479fe8a67e
taeyoung0524 <miseoseo1@naver.com>
2025-08-15 14:53:43 +0900

    FEAT : App.js 초기 세팅

--- itzi/src/App.js
+++ itzi/src/App.js
@@ -1,7 +1,13 @@
+import Footer from "./components/Commom_Section/Footer"
+import Nav from "./components/Commom_Section/Nav"
+import Main from "./components/Main"
+
 const App = () => {
   return (
     <div>
-      
+      <Nav/>
+      <Main/>
+      <Footer/>
     </div>
   )
 }
