5c90cad48626ebe388de369afa7e9c8c83d67dc0
taeyoung0524 <miseoseo1@naver.com>
2025-08-17 03:15:24 +0900

    FEAT : 3 header 구현

--- itzi/src/pages/detailpage/StoreDetail.jsx
+++ itzi/src/pages/detailpage/StoreDetail.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import prior from "../../assets/img/priorbutton.png"
 
 const StoreDetail = () => {
   return (
-    <div>
-      가게 세부 정보 페이지
+    <div className="storeDetail">
+      <div className="header">
+        <img src={prior} alt="" />
+        <div className="scrap">
+            <h3>성신여자대학교</h3>
+            <h3>컴퓨터공학과</h3>
+            <h3>AI 학과</h3>
+        </div>
+        <p>학생들이 많이 스크랩했어요!</p>
+      </div>
+      <div className="container">
+
+      </div>
     </div>
   )
 }
